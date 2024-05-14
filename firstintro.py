0ab64a963d3194d4a15f36521f858b22454b944d
Mason Krause <masongkrause@yahoo.com>
2024-05-14 16:33:29 -0500

    Initial commit

---
+++ firstintro.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+#INITIALIZING NP ARRAY FROM A PYTHON LIST
+a = np.array([1,2,3,4,5,6])
+print(a[1])#should be 2
+
+a = np.array([[1,2,3,4],[5,6,7,8],[9,10,11,12]])
+print(a[0])#should be [1,2,3,4]
+
+'''
+ndarray is the np class used to represent matrices and vectors
+vectors are single dimension (1D) arrays
+matrices are 2D arrays
+tensors are 3D and higher dimensional arrays
+'''
+
+'''
+What are the attributes of an array
+'''
+
